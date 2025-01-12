45c409526ab3470c62b2e89da8c98ae43c5dcf70
AndreyAndreyQ11 <aayy6491@gmail.com>
2025-01-12 18:17:47 +0200

    тест 5

--- src/component/App.jsx
+++ src/component/App.jsx
@@ -31,43 +31,96 @@ export default class App extends Component {
     ],
 
     flyPicture: [], //та шо летает
-    // stickPicture: false,
+
     flightPictures: {
       x: "",
       y: "",
+
     },
     boundingRect: null,
+    down: false,
   }
 
-  sticksUp = (el) => {
-    this.setState({
-      flightPictures: {
-        x: el.nativeEvent.layerX,
-        y: el.nativeEvent.layerY,
-      }
-    });
-  };
+
 
   // объект координат элемента при загрузке и обновлении экрана boundingRect = document.querySelector(`.${s.container}`).getBoundingClientRect();
   componentDidMount() {
     const boundingRect = document.querySelector(`.${s.container}`).getBoundingClientRect();
     this.setState({ boundingRect }); // Сохраняем размеры контейнера в state
-
     window.addEventListener('resize', this.updateBoundingRect); // Обновляем boundingRect при изменении окна
+
+    //глобальные слушатели события mouseup и touchend
+    window.addEventListener('mouseup', this.mouseUp);
+    window.addEventListener('touchend', this.mouseUp);
+  }
+  componentWillUnmount() {
+    window.removeEventListener('resize', this.updateBoundingRect); // Очищаем обработчик при размонтировании
+
+    // Удаляем глобальные слушатели
+    window.removeEventListener('mouseup', this.mouseUp);
+    window.removeEventListener('touchend', this.mouseUp);
   }
   updateBoundingRect = () => {
     const boundingRect = document.querySelector(`.${s.container}`).getBoundingClientRect();
     this.setState({ boundingRect });
   };
-  componentWillUnmount() {
-    window.removeEventListener('resize', this.updateBoundingRect); // Очищаем обработчик при размонтировании
+
+  // функция при нажатии
+  stickPicture = (pictures) => {
+    const { top, flyPicture, } = this.state
+
+    //переносим выбраную картинку в другой массив
+    const f2 = f1(top, pictures);
+    this.setState({
+      flyPicture: f2[0],
+      top: f2[1],
+      down: true,
+    });
+
+  };
+  //
+  mouseUp = () => {
+    console.log(!this.state.down);
+    if (!this.state.down) return
+
+    const pushPictures = (top, flyPicture) => {
+
+      let newTop = [...top]
+
+
+      let numberStringhtPush = [0, 0,]// дописать надо
+
+      // if (numberStringhtPush >= 0 && numberStringhtPush < newTop.length) {
+      //   newTop[numberStringhtPush].push(flyPicture);
+      // }
+
+
+
+      // Вставляем flyPicture в нужную позицию подмассива
+      newTop[numberStringhtPush[0]].splice(numberStringhtPush[1], 0, flyPicture); // splice добавляет элемент в нужную позицию
+
+
+
+
+      return newTop;
+    };
+
+    this.setState({
+      top: pushPictures(this.state.top, this.state.flyPicture[0]),
+      flyPicture: [],
+      down: false,
+    });
+
   }
 
-  // функция срабатывает раз в 10ед
-  mouseMove = debounce((el) => {
-    if (!this.state.boundingRect) return;
 
-    const { boundingRect } = this.state;
+  // функция срабатывает раз в 10ед вычесляет координаты
+  mouseMove = debounce((el) => {
+    const { boundingRect, down } = this.state;
+
+    if (!boundingRect) return;
+
+
     let x = 0, y = 0;
 
     // Если из телефона иначе из кампютера
@@ -86,32 +139,18 @@ export default class App extends Component {
     // console.log(`x: ${x}, y: ${y}`);
   }, 10);
 
-  // stickPicture = (el) => {
-  //   console.log(el);
-  //   ///
-  // };
-
-  stickPicture = (pictures) => {
-    const { top, flyPicture } = this.state
-
-    //переносим выбраную картинку в другой массив
-    const f2 = f1(top, pictures);
-    this.setState({
-      flyPicture: f2[0],
-      top: f2[1],
-    });
-
-  };
 
   render() {
     const { top, arreyTopKeys, flyPicture, flightPictures } = this.state
-    const { stickPicture } = this
+    const { stickPicture, } = this
 
     return (
       <div
         className={s.container}
         onMouseMove={this.mouseMove}
         onTouchMove={this.mouseMove}
+        onMouseUp={this.mouseUp}
+        onTouchEnd={this.mouseUp}
       >
         {
           top.slice(0, arreyTopKeys.length).map((item, i) => {
